3af747f8c248198d29de840f1e5afd3873710d03
shajingzhe <384673944@qq.com>
2023-11-21 22:06:21 +0800

    redis key分级

--- src/test/java/com/xspace/redisdemo/RedisDemoApplicationTests.java
+++ src/test/java/com/xspace/redisdemo/RedisDemoApplicationTests.java
@@ -1,9 +1,12 @@
 package com.xspace.redisdemo;
 
+import cn.hutool.json.JSONUtil;
+import com.xspace.redisdemo.dto.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class RedisDemoApplicationTests {
@@ -11,13 +14,16 @@ class RedisDemoApplicationTests {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    private String key="user:model:49";
     @Test
     void testString() {
+        User user=new User("哈哈",45);
+
         // 写入一条String数据
-        redisTemplate.opsForValue().set("name", "虎哥");
+        redisTemplate.opsForValue().set(key, user);
         // 获取string数据
-        Object name = redisTemplate.opsForValue().get("name");
-        System.out.println("name = " + name);
+        User user1 = (User) redisTemplate.opsForValue().get(key);
+        System.out.println(user1);
     }
 
 }
